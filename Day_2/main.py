4e92ac5bb31463184760034889157f2e8c6acc5b
Limetofu <tkfkdgo6327@gmail.com>
2023-03-10 12:01:34 +0900

    first commit

---
+++ Day_2/main.py
@@ -0,0 +1,11 @@
+n = 1
+
+while n <= 1000:
+    if n % 3 == 0:
+        print(n)
+    n += 1
+
+for i in range(1, 1000 + 1):
+    if i % 3 == 0:
+        print(i)
+
---
+++ Day_3/main.py
@@ -0,0 +1,8 @@
+print('hello', end=' ')  # end의 basic값은 개행문자.  
+print('world')
+
+print('cats', 'dogs', 'mice', sep=':')  # seperate argument 인자 사이에 넣을 수 있음
+
+
+######################################################################################3333
+# Global / Local Scope
